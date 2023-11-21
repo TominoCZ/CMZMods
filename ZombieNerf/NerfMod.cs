24c200d6599890d28697a4c0a856786b482c95bc
TOM-PC\tom <tom@dlnet.us>
2023-11-21 20:40:07 +0100

    ZombieNerf mod now works

--- ZombieNerf/NerfMod.cs
+++ ZombieNerf/NerfMod.cs
@@ -1,6 +1,9 @@
-﻿using DNA.CastleMinerZ;
+﻿using DNA;
+using DNA.CastleMinerZ;
 using DNA.CastleMinerZ.AI;
+using DNA.CastleMinerZ.Terrain;
 using DNA.CastleMinerZ.UI;
+using DNA.CastleMinerZ.Utils.Trace;
 using DNA.Drawing;
 using DNA.Drawing.UI;
 using HarmonyLib;
@@ -9,33 +12,66 @@ using Modding;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NerfMod
 {
-    /*
     [HarmonyPatch]
     static class Patch
     {
-        [HarmonyFinalizer, HarmonyPatch(typeof(ZombieDig), "Enter")]
-        static void Enter(ZombieDig __instance, BaseZombie ___entity)
+        [HarmonyPrefix, HarmonyPatch(typeof(EnemyManager), "AttentuateVelocity")]
+        static bool BypassSlowdown(out float __result)
         {
-            if (___entity.EType.AttackAnimationSpeed == ___entity.CurrentPlayer.Speed)
-                ___entity.CurrentPlayer.Speed = 1;
+            __result = 1;
+
+            return false;
         }
-    }*/
+
+        /* USE THIS TO USE DEFAULT LOGIC IF YOU WANT TO MODIFY IT
+        [HarmonyPrefix, HarmonyPatch(typeof(EnemyManager), "AttentuateVelocity")]
+        static bool AttentuateVelocity(EnemyManager __instance, List<BaseZombie> ____enemies, Player plr, Vector3 fwd, Vector3 worldPos, out float __result)
+        {
+            float num = 1f;
+            for (int i = 0; i < ____enemies.Count; i++)
+            {
+                if (____enemies[i].Target != plr || !____enemies[i].IsBlocking)
+                {
+                    continue;
+                }
+
+                Vector3 value = ____enemies[i].WorldPosition - worldPos;
+                float num2 = value.LengthSquared();
+                float num3 = 1f;
+                if ((double)num2 < 4.0 && (double)Math.Abs(value.Y) < 1.5)
+                {
+                    num3 = 0.5f;
+                    if ((double)num2 > 9.99999974737875E-05)
+                    {
+                        float num4 = Vector3.Dot(Vector3.Normalize(value), fwd);
+                        if ((double)num4 > 0.0)
+                        {
+                            num3 *= Math.Min(1f, (float)(2.0 * (1.0 - (double)num4)));
+                        }
+                    }
+                }
+
+                num *= num3;
+            }
+
+            __result = num;
+
+            return false;
+        }*/
+    }
 
     public class NerfMod : ModBase<NerfMod, CastleMinerZGame>
     {
         public NerfMod(CastleMinerZGame game) : base(game, "ZombieNerf", "com.Morphox.ZombieNerf")
         {
 
-        }
-
-        public override void LoadMain()
-        {
-            this.Log("This mod doesn't work yet.", LogType.Warn);
         }
     }
 }
